a0ea1d049e7c0d25612bdbb135257afe229389f4
aabenjamim <alanebenjamimdossantos@gmail.com>
2023-04-13 00:09:02 -0300

    versão com post messages incompleto

--- src/app.js
+++ src/app.js
@@ -20,6 +20,9 @@ mongoClient.connect()
     .catch((err) => console.log(err.message))
 
 // Endpoints
+
+const time = `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}`
+
 app.post('/participants', (req, res)=>{
     const {name} = req.body
 
@@ -35,12 +38,48 @@ app.post('/participants', (req, res)=>{
         to: 'Todos', 
         text: 'entra na sala...', 
         type: 'status', 
-        time: `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}`
+        time: time
     }
     
     db.collection('messages').insertOne(entrou)
         .then(() => res.sendStatus(201))
         .catch((err) => res.status(500).send(err.message))
+})
+
+app.get("/participants", (req, res) => {
+    db.collection('participants').find().toArray()
+        .then((participantes) => res.status(200).send(participantes))
+        .catch((err) => res.status(500).send(err.message))
+})
+
+app.post('/messages', (req, res)=>{
+    const {to, text, type} = req.body
+
+    const { User } = req.headers
+
+    const msg = {
+        from: User,
+        to,
+        text,
+        type,
+        time: time
+    }
+
+    let ok = false
+
+    db.collection('participants').findOne(User)
+        .then(()=>{
+            ok = true
+            res.sendStatus()
+        })
+        .catch((err) => res.status(422).send(err.message))
+
+    if(ok){
+        db.collection('messages').insertOne(msg)
+            .then(()=>res.sendStatus(201))
+            .catch((err) => res.status(500).send(err.message))
+    }
+
 })
 
 // Deixa o app escutando, à espera de requisições
